5440aa2a41d6cb6446ce6905da7af30196aad941
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-13 02:41:27 +0900

    feat: 무한 스크롤 기능 추가

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -3,26 +3,40 @@ import { ChangeEvent, FormEvent, MouseEvent, useEffect, useRef } from "react";
 import { Button, Col, Form, Input, Row } from "reactstrap";
 import SearchService from "../api/SerchService";
 import useSearchDispatch from "../hooks/useSearchDispatch";
+import useSearchState from "../hooks/useSearchState";
+import { debounce } from "../utils";
 import Assosiate from "./Assosiate";
 
-const debounce = (fn: (...arg: any[]) => void, delay: number) => {
-  let timerId: NodeJS.Timeout | null = null;
-  return (...arg: any[]) => {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-    timerId = setTimeout(fn.bind(null, ...arg), delay);
-  };
-};
 type InputDom = HTMLInputElement | null;
 
 const InputComponent = () => {
   const dispatch = useSearchDispatch();
   const inputRef = useRef<InputDom>(null);
+	const {start} = useSearchState()
+
+	const handleScroll = (e: any) => {
+		const { clientHeight, scrollTop, scrollHeight } = e.target.scrollingElement;
+		if (clientHeight + scrollTop === scrollHeight) {
+			const nextStart = start + 1
+			const search = inputRef.current!.value;
+			SearchService.getSearchResult(search, nextStart)
+				.then(data => {
+					dispatch({type: "ADD_SEARCH_RESULT", payload: data})
+					dispatch({type:"SET_START", payload: nextStart})
+				})
+		}
+	}
+	const debounceScroll = debounce(handleScroll, 300)
 
   useEffect(() => {
     inputRef.current = document.querySelector<HTMLInputElement>(".inputSearch");
   }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", debounceScroll, { passive: true});
+		return () => {
+			window.removeEventListener("scroll", debounceScroll)
+		}
+	}, [debounceScroll])
 
   const getAssociate = async (e: ChangeEvent<HTMLInputElement>) => {
     const { value: search } = e.target;
@@ -38,6 +52,7 @@ const InputComponent = () => {
     const data = await SearchService.getSearchResult(search);
 
     dispatch({ type: "GET_SEARCH_RESULT", payload: data });
+		dispatch({type: "SET_START", payload: 1})
     dispatch({ type: "CLEAR_ASSOCIATE" });
   };
 
