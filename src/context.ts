0c92b5a9ca87eeb34a3a149029fd4eff042b0234
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-13 02:37:55 +0900

    feat: cahrt 관련 context 추가

--- src/context.ts
+++ src/context.ts
@@ -1,9 +1,11 @@
 import { createContext, Dispatch } from "react";
-import { Compelete, SearchResult } from "./type";
+import { Compelete, Product, SearchResult } from "./type";
 
 export type State = {
   associate: Compelete;
   searchResult: SearchResult;
+  chartDetail: Product[];
+  start: number;
 };
 
 export type Action =
@@ -17,6 +19,24 @@ export type Action =
   | {
       type: "GET_SEARCH_RESULT";
       payload: SearchResult;
+    }
+  | {
+      type: "ADD_SEARCH_RESULT";
+      payload: SearchResult;
+    }
+  | {
+      type: "CLEAR_SEARCH_RESULT";
+    }
+  | {
+      type: "GET_CATEGORY_DETAIL";
+      payload: Product[];
+    }
+  | {
+      type: "CLEARE_CATEGORY_DETAIL";
+    }
+  | {
+      type: "SET_START";
+      payload: number;
     };
 
 export type SearchDispatch = Dispatch<Action>;
@@ -44,6 +64,45 @@ export const reducer = (state: State, action: Action): State => {
       return {
         ...state,
         searchResult: action.payload,
+      };
+    case "ADD_SEARCH_RESULT":
+      console.log("state", state.searchResult.items);
+      console.log("action", action.payload.items);
+      const items = [...state.searchResult.items, ...action.payload.items];
+      console.log("newItems", items);
+
+      return {
+        ...state,
+        searchResult: {
+          ...state.searchResult,
+          items,
+        },
+      };
+    case "CLEAR_SEARCH_RESULT":
+      return {
+        ...state,
+        searchResult: {
+          display: 0,
+          items: [],
+          lastBuildDate: "",
+          start: 0,
+          total: 0,
+        },
+      };
+    case "GET_CATEGORY_DETAIL":
+      return {
+        ...state,
+        chartDetail: action.payload,
+      };
+    case "CLEARE_CATEGORY_DETAIL":
+      return {
+        ...state,
+        chartDetail: [],
+      };
+    case "SET_START":
+      return {
+        ...state,
+        start: action.payload,
       };
     default:
       throw new Error("Unhandled action");
